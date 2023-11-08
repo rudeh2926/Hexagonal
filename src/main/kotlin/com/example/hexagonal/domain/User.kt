8978bba32b9d16a398e5354f22c9cc1bb3b0af54
도경 <dokyungkim2926@gmail.com>
2023-11-08 11:14:08 +0900

    user Entity

--- src/main/kotlin/com/example/hexagonal/domain/User.kt
+++ src/main/kotlin/com/example/hexagonal/domain/User.kt
@@ -1,4 +1,22 @@
 package com.example.hexagonal.domain
 
-class User {
-}
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class User (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Long,
+
+    @Column(nullable = false)
+    val accountId : String,
+
+    @Column(nullable = false)
+    val password : String,
+
+    @Column(nullable = false)
+    val name : String
+)
