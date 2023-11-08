1fbb792e90b0ee72bf63e8add58db77dca9e9c4d
도경 <dokyungkim2926@gmail.com>
2023-11-08 13:45:08 +0900

    application out 작성

---
+++ src/main/kotlin/com/example/hexagonal/application/port/out/UserRepositoryPort.kt
@@ -0,0 +1,9 @@
+package com.example.hexagonal.application.port.out
+
+import com.example.hexagonal.domain.User
+
+interface UserRepositoryPort {
+    fun saveUser(user: User)
+
+    fun findByEmail(email : String)
+}
